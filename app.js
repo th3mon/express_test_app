a16bd9339e4dc7d04a1c24cf32495a3c96eb97b1
Przemysław Szelenberger <p.szelenberger@gmail.com>
2013-07-23 11:00:09 +0200

    Simple app

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+    app = express(),
+    hbs = require("hbs"),
+    blogEngine = require("./blog");
+
+app.set("view engine", "html");
+app.engine("html", hbs.__express);
+app.use(express.bodyParser());
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index", {title: "My Blog", entries: blogEngine.getBlogEntries()});
+});
+
+app.get("/about", function(req, res) {
+    res.render("about", {title: "About Me"});
+});
+
+app.get("/article/:id", function(req, res) {
+    var entry = blogEngine.getBlogEntry(req.params.id);
+    res.render("article", {title: entry.title, blog: entry});
+});
+
+app.listen(3000);
